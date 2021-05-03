058bc51718883f7e12117ef6b64fc9073e046b99
Annu Maria Thomas <36506894+Annumaria@users.noreply.github.com>
2021-05-04 01:24:28 +0530

    Create demonstrate Adding elements to an array using array module.java

---
+++ Arrays/demonstrate Adding elements to an array using array module.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	   int x,pos,loop=1;
+      ArrayList<Integer> a=new ArrayList<Integer>();
+      System.out.print("Enter number of elements: ");
+      Scanner sc= new Scanner(System.in);
+      int n =sc.nextInt();
+      System.out.println("Enter the elements: ");
+      for(int i=0;i<n;i++){
+          x=sc.nextInt();
+          a.add(x);
+      }
+      
+      
+      System.out.print("Enter the position to add the element : ");
+     pos=sc.nextInt();
+     while(loop==1){
+         System.out.println("Enter the element: ");
+          x=sc.nextInt();
+           a.add(pos-1,x);
+         System.out.println("To continue press 1 else press any number !");
+         loop=sc.nextInt();
+         if(loop==1){
+           System.out.print("Enter the position to add the element : ");  
+           pos=sc.nextInt();
+         }
+         
+     }
+     
+      System.out.print("Final array : "); 
+      for(int i=0;i<a.size();i++){
+           System.out.print(a.get(i)+" ");
+      }
+     
+	}
+   
+}
