62b443b5ddb3ec2436132dd088a47e51d6d77f67
Annu Maria Thomas <36506894+Annumaria@users.noreply.github.com>
2021-05-04 21:52:46 +0530

    Create print elements at odd position.java

---
+++ Arrays/print elements at odd position.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+     int n;
+     System.out.print("Enter the number of elements :");
+     Scanner sc=new Scanner(System.in);
+     n=sc.nextInt();
+     
+     int[] a=new int[n];
+     System.out.println("Enter the elements :");
+     for(int i=0;i<n;i++){
+         a[i]=sc.nextInt();
+     }
+     
+     System.out.println("Elements at odd position : ");
+     for(int i=0;i<n;i+=2){
+         System.out.print(a[i]+" ");
+     }
+    }
+}
