6faa17dafb3a10d1b34477ca661eb8019b5c812d
Annu Maria Thomas <36506894+Annumaria@users.noreply.github.com>
2021-05-04 17:31:11 +0530

    Create print the largest element in an array.java

---
+++ Arrays/print the largest element in an array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+      int n,temp=0;
+      System.out.print("Enter the number of elements: ");
+      Scanner sc=new Scanner(System.in);
+      n=sc.nextInt();
+      System.out.println("Enter the elements: ");
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=sc.nextInt();
+      }
+       for(int i=0;i<n;i++){
+          for(int j=i+1;j<n;j++){
+              if(a[i]>a[j]){
+                  temp=a[i];
+                  a[i]=a[j];
+                  a[j]=temp;
+              }
+          }
+      }
+      System.out.print("Largest Element = "+a[n-1]);
+    }
+}
