39fe7ffa6ac5fc12e2176c80e8efccf73475a29f
Annu Maria Thomas <36506894+Annumaria@users.noreply.github.com>
2021-05-04 11:46:29 +0530

    Create sort the elements of an array in descending order.java

---
+++ Arrays/sort the elements of an array in descending order.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+      int n,temp;
+      System.out.print("Enter the number of elements: ");
+      Scanner sc=new Scanner(System.in);
+      n=sc.nextInt();
+      System.out.println("Enter the elements: ");
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=sc.nextInt();
+      }
+      
+      for(int i=0;i<n;i++){
+          for(int j=i;j<n;j++){
+              if(a[i]>a[j]){
+                  temp=a[i];
+                  a[i]=a[j];
+                  a[j]=temp;
+              }
+          }
+      }
+      
+      System.out.print("Sorted array : ");
+      for(int i=0;i<n;i++){
+          System.out.print(a[i]+" ");
+      }
+    }
+}
