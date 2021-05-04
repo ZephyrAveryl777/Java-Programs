a224f3849863c74fe4120d5a448c4f4c9869522b
Annu Maria Thomas <36506894+Annumaria@users.noreply.github.com>
2021-05-04 11:52:18 +0530

    Update and rename sort the elements of an array in descending order.java to sort the elements of an array in ascending order.java

--- Arrays/sort the elements of an array in ascending order.java
+++ Arrays/sort the elements of an array in ascending order.java
@@ -13,7 +13,7 @@ public class MyClass {
       }
       
       for(int i=0;i<n;i++){
-          for(int j=i;j<n;j++){
+          for(int j=i+1;j<n;j++){
               if(a[i]>a[j]){
                   temp=a[i];
                   a[i]=a[j];
